98fb715b008a3557a77a9a53c28b5b0ada28e9f4
朱志强 <hzzhuzhiqiang@corp.netease.com>
2022-03-20 17:51:00 +0800

    Initial Commit

---
+++ Shared/RecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoderApp.swift
+//  Shared
+//
+//  Created by 朱志强 on 2022/3/20.
+//
+
+import SwiftUI
+
+@main
+struct RecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
